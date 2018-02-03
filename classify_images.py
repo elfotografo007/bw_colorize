61efcb14df284efe90f6fc0f53a3b3a95992ac54
Andres Torres <elfotografo007@gmail.com>
2018-02-03 10:38:15 +0100

    Implemented classification script to speed up everything

---
+++ classify_images.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from keras.applications.inception_resnet_v2 import preprocess_input, InceptionResNetV2
+from skimage.transform import resize
+
+#Load weights
+inception = InceptionResNetV2(weights='imagenet', include_top=True)
+
+# Use the model, put a image in the function and return the classification output
+def classification_model(grayscaled_rgb):
+    grayscaled_rgb_resized = np.zeros((grayscaled_rgb.shape[0], 299, 299, 3))
+    for i in range(grayscaled_rgb.shape[0]):
+        g = resize(grayscaled_rgb[i], (299, 299, 3), mode='constant')
+        grayscaled_rgb_resized[i] = g
+    grayscaled_rgb_resized = preprocess_input(grayscaled_rgb_resized)
+    classification_result = inception.predict(grayscaled_rgb_resized)
+    return classification_result
+
+training_rgb = np.load('/home/s1821105/AML/training_labels.npy')
+training_classification = classification_model(training_rgb)
+np.save('/home/s1821105/AML/training_classification', training_classification)
+del training_classification, training_rgb
+
+validation_rgb = np.load('/home/s1821105/AML/validation_labels.npy')
+validation_classification = classification_model(validation_rgb)
+np.save('/home/s1821105/AML/validation_classification', validation_classification)
+del validation_rgb, validation_classification
+
+test_rgb = np.load('/home/s1821105/AML/test_labels.npy')
+test_classification = classification_model(test_rgb)
+np.save('/home/s1821105/AML/test_classification', test_classification)
